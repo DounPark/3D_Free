499bdd27433530ea316a74f7756010149fdbf2f8
DounPark <ehdns1562@gmail.com>
2025-06-10 10:08:47 +0900

    add manythings

---
+++ Assets/Scripts/StateMachine/PlayerBaseState.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBaseState : IState
+{
+    protected PlayerStateMachine stateMachine;
+    protected readonly PlayerGroundData groundData;
+
+    public PlayerBaseState(PlayerStateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+        groundData = stateMachine.Player.Data.GroundData;
+    }
+    
+    public virtual void Enter()
+    {
+        AddInputActionsCallbacks();
+    }
+
+    public virtual void Exit()
+    {
+        RemoveInputActionsCallbacks();
+    }
+
+    protected virtual void AddInputActionsCallbacks()
+    {
+        PlayerController input = stateMachine.Player.Input;
+        input.playerActions.Movement.canceled += OnMovementCanceled;
+        input.playerActions.Run.started += OnRunStarted;
+        input.playerActions.Jump.started += OnJumpStarted;
+    }
+    
+    protected virtual void RemoveInputActionsCallbacks()
+    {
+        PlayerController input = stateMachine.Player.Input;
+        input.playerActions.Movement.canceled -= OnMovementCanceled;
+        input.playerActions.Run.started -= OnRunStarted;
+        input.playerActions.Jump.started -= OnJumpStarted;
+    }
+
+    public virtual void HandleInput()
+    {
+        ReadMovementInput();
+    }
+
+    public virtual void Update()
+    {
+        Move();
+    }
+
+    public virtual void PhysicsUpdate()
+    {
+        
+    }
+    
+    protected virtual void OnMovementCanceled(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    protected virtual void OnRunStarted(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    protected virtual void OnJumpStarted(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    protected void StartAnimation(int animatorHash)
+    {
+        stateMachine.Player.Animator.SetBool(animatorHash, true);
+    }
+    
+    protected void StoptAnimation(int animatorHash)
+    {
+        stateMachine.Player.Animator.SetBool(animatorHash, false);
+    }
+
+    private void ReadMovementInput()
+    {
+        stateMachine.MovementInput = stateMachine.Player.Input.playerActions.Movement.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        Vector3 movementDirection = GetMovementDirection();
+        
+        Move(movementDirection);
+        Rotate(movementDirection);
+    }
+
+    private Vector3 GetMovementDirection()
+    {
+        Vector3 forward = stateMachine.MainCamTransform.forward;
+        Vector3 right = stateMachine.MainCamTransform.right;
+        
+        forward.y = 0;
+        right.y = 0;
+        
+        forward.Normalize();
+        right.Normalize();
+        
+        return forward * stateMachine.MovementInput.y + right * stateMachine.MovementInput.x;
+    }
+
+    private void Move(Vector3 direction)
+    {
+        float movement = GetMovementSpeed();
+        stateMachine.Player.Controller.Move(((movement * direction) + stateMachine.Player.ForceReceiver.Movement) * Time.deltaTime);
+    }
+
+    private float GetMovementSpeed()
+    {
+        float moveSpeed = stateMachine.MovementSpeed * stateMachine.MovementSpeedModifier;
+        return moveSpeed;
+    }
+
+    private void Rotate(Vector3 direction)
+    {
+        if (direction != Vector3.zero)
+        {
+            Transform playerTransform = stateMachine.Player.transform;
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            playerTransform.rotation = Quaternion.Slerp(playerTransform.rotation, targetRotation, Time.deltaTime * stateMachine.RotationDamping);
+        }
+    }
+}
